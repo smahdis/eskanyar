efe8de35978dfaab0a51c28d04fbd16c2f9897a9
Mahdi <Mahdi@above.ge>
2024-09-01 16:18:49 +0330

    final touches

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use Kavenegar\Exceptions\ApiException;
 use Kavenegar\Laravel\Message\KavenegarMessage;
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+    return Redirect::to('/admin');
 });
 
 
